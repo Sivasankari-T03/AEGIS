ddf3230d0afe98cd5098aa88b02438f5a16d4140
Sivasankari-T03 <sivasankari.t.2024.csecs@rajalakshmi.edu.in>
2026-09-15 19:31:52 +0530

    Implemented Sudoku Backtracking solver and API

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from classifier import classify_problem
 from algorithms.bfs import bfs
 from algorithms.dfs import dfs
 from algorithms.astar import astar
+from algorithms.backtracking import solve
 
 app = FastAPI(title="AEGIS")
 
@@ -52,4 +53,25 @@ def optimal_route(start: str, goal: str):
         "goal": goal,
         "optimal_path": path,
         "total_cost": cost
+    }
+@app.get("/sudoku")
+def sudoku_demo():
+
+    board = [
+        [5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9]
+    ]
+
+    solve(board)
+
+    return {
+        "algorithm":"Backtracking",
+        "solution":board
     }
